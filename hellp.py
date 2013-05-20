3c114f4c83755ad00156c37130e02fe6e1d4a444
dreamonxu <dreamonxu@gmail.com>
2013-05-20 17:17:41 +0800

    first cheerrypy

--- hellp.py
+++ hellp.py
@@ -1,2 +1,12 @@
-import sysbo
-dir(sysbo)
+import cherrypy
+class OnePage(object):
+    def index(self):
+        return "one page!"
+    index.exposed = True
+class HelloWorld(object):
+    onepage=OnePage()
+    def index(self):
+        return "hello world"
+    index.exposed=True
+
+cherrypy.quickstart(HelloWorld())
